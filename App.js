0fb0ed638b767cdb87e9667c6fa99adc568d5de2
wald-d <48999686+wald-d@users.noreply.github.com>
2020-02-25 14:23:59 -0800

    Inital implementation

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Font from "expo-font";
+import React, { Component } from 'react';
+import { View, Button, FlatList, Text} from 'react-native';
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
 
-import ParkingAppNavigator from './navigation/ParkingAppNavigator';
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
-  });
-};
 
-export default function App() {
-  return (
-      <ParkingAppNavigator />
-  );
+export default class DatabaseComponent extends Component {
+  state = {
+    parkingLots: []
+  };
+
+  componentDidMount() {
+    firebase.database().ref('parkingLots/').on('value', snapshot => {
+      let data = snapshot.val();
+      let parkingLots = Object.values(data);
+      this.setState({ parkingLots });
+    });
+  }
+  render() {
+    return (
+      <View style={{flex: 1, marginTop: Platform.OS === 'ios' ? 34 : 0}}>
+        <View style={{
+          backgroundColor: 'green',
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          height: 64
+        }}>
+         <Button title="set" onPress={() => this.writeUserData('@gmail', 'D', 'w')} />
+         <Button title="get" onPress={() => this.readUserData()} />
+        </View>
+
+        <FlatList
+          data={this.state.parkingLots}
+          renderItem={({item}) => 
+            <Text style={{
+              marginTop: 98,
+              padding: 10,
+              fontSize: 18,
+              height: 44,}}>
+                {item.LOT_ID}
+            </Text>}
+        />
+      </View>
+    );
+  }
 }
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBbI2IcZ_hQGG5QUj_ufuh0LhmnMn2tbEg",
+    authDomain: "testproject-b4733.firebaseapp.com",
+    databaseURL: "https://testproject-b4733.firebaseio.com",
+    projectId: "testproject-b4733",
+    storageBucket: "testproject-b4733.appspot.com",
+    messagingSenderId: "1036147926561",
+    appId: "1:1036147926561:web:4e1c932363e488260e6560",
+    measurementId: "G-SYW8B059G2"
+  };
